718d2579eac4f4ec6553995f7d597733b4826b47
Constructor <fractalminds@protonmail.com>
2021-07-13 00:28:45 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4f4f07b2786f411c6e4864efe001bdc8.js"
+  "./precache-manifest.2021345acb9a7193c7160d010abedddb.js"
 );
 
 self.addEventListener('message', (event) => {
